36fa498001103b21a6b85e75c9024948f71e41ff
Andri Firman Saputra <65483768+hako-975@users.noreply.github.com>
2023-10-27 00:45:35 +0700

    Update test_fuzzy.py

--- tests/test_fuzzy.py
+++ tests/test_fuzzy.py
@@ -11,44 +11,7 @@ def test_freeze():
     assert temp.freeze(40) == 0
     assert temp.freeze(20) == 1
 
-def test_cold():
-    temp = Temp()
-    assert temp.cold(30) == 0
-    assert temp.cold(50) == 0.5
-    assert temp.cold(70) == 0.5
-    assert temp.cold(80) == 0
-    assert temp.cold(90) == 0
-
-def test_warm():
-    temp = Temp()
-    assert temp.warm(60) == 1
-    assert temp.warm(70) == 0.5
-    assert temp.warm(50) == 0.5
-    assert temp.warm(80) == 0
-    assert temp.warm(90) == 0
-
-def test_hot():
-    temp = Temp()
-    assert temp.hot(80) == 1
-    assert temp.hot(90) == 0.5
-    assert temp.hot(70) == 0.5
-    assert temp.hot(100) == 1
-    assert temp.hot(110) == 0
-
-# Test Pressure class
-def test_very_low():
-    pressure = Pressure()
-    assert pressure.very_low(0.1) == 1
-    assert pressure.very_low(0.3) == 0
-    assert pressure.very_low(0.15) == 1
-
-def test_low():
-    pressure = Pressure()
-    assert pressure.low(0.3) == 0
-    assert pressure.low(0.5) == 0.5
-    assert pressure.low(0.7) == 0.5
-    assert pressure.low(0.6) == 0
-    assert pressure.low(0.8) == 0
+# ... (other test functions for Temp)
 
 # Test Speed class
 def test_calculate_speed():
@@ -56,6 +19,23 @@ def test_calculate_speed():
     assert speed.calculate_speed("FREEZE", "VERY LOW") == [80, 100, 100]
     assert speed.calculate_speed("COLD", "HIGH") == [80, 100, 100]
     # Add more test cases for different combinations of temperature and pressure.
+
+# Mock user input for tests
+@pytest.mark.parametrize("temperature, pressure, expected_speed", [
+    ("FREEZE", "VERY LOW", [80, 100, 100]),
+    ("COLD", "HIGH", [80, 100, 100]),
+    # Add more test cases here
+])
+def test_speed_calculation_with_input(monkeypatch, temperature, pressure, expected_speed):
+    # Mock user input
+    monkeypatch.setattr('builtins.input', lambda _: f"{temperature}\n{pressure}\n")
+    
+    # Now you can run your script
+    speed = Speed()
+    speed_values = speed.calculate_speed(temperature, pressure)
+    
+    # Assertions
+    assert speed_values == expected_speed
 
 # You can also add test functions for the graphing functionality if you want to test the plotting.
 
